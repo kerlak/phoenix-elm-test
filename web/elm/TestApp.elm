63b2fd42ebf7cc626e4ebe227ecfacda41283ac6
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2017-03-07 15:40:31 +0100

    Added elm integration, render an elm module with a countdown

---
+++ web/elm/TestApp.elm
@@ -0,0 +1,109 @@
+module TestApp exposing (..)
+import Html exposing (Html, Attribute, div, text)
+import Html.Attributes exposing (..)
+import Time exposing (Time, second)
+import Date exposing (Date)
+
+main =
+  Html.program
+    { init = model
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+-- MODEL
+
+type alias Model =
+  { currentTime : Time
+  , goalTime : Time
+  }
+
+goalTimeString = "2017-04-21 14:00:00"
+goalTime = createTime goalTimeString
+
+model : (Model, Cmd Msg)
+model =
+  (Model 0 goalTime, Cmd.none)
+
+-- UPDATE
+
+type Msg
+  = Tick Time
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      (Model newTime model.goalTime, Cmd.none)
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every second Tick
+
+-- UTIL FUNCTIONS
+
+twoDigitString number =
+  if number < 10 then
+    toString number
+    |> String.append "0"
+  else
+    toString number
+
+createTime str =
+  Date.fromString str
+  |> Result.withDefault (Date.fromTime 0)
+  |> Date.toTime
+
+-- VIEW
+myStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "100%")
+    , ("font-family", "Arial")
+    ]
+
+view : Model -> Html Msg
+view model =
+  let
+    delta =
+      model.goalTime - model.currentTime
+
+    ndays =
+      Time.inHours delta
+      |> floor
+      |> (flip (//)) 24
+
+    nhours =
+      Time.inHours delta
+      |> floor
+      |> (flip (-)) (ndays * 24)
+
+    nminutes =
+      Time.inMinutes delta
+      |> floor
+      |> (flip (-))(ndays * 24 * 60)
+      |> (flip (-))(nhours * 60)
+
+    nseconds =
+      Time.inSeconds delta
+      |> floor
+      |> (flip (-))(ndays * 24 * 60 * 60)
+      |> (flip (-))(nhours * 60 * 60)
+      |> (flip (-))(nminutes * 60)
+
+    countdown =
+      String.append (twoDigitString ndays)
+      <| String.append " "
+      <| String.append (twoDigitString nhours)
+      <| String.append ":"
+      <| String.append (twoDigitString nminutes)
+      <| String.append ":"
+      <| (twoDigitString nseconds)
+
+  in
+    div [ ] [
+        div [ ] [ text countdown ]
+    ]
