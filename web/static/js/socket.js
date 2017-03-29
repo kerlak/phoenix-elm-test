ed304b98ff0c3613139386abca59b22e069f3de6
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2017-03-29 13:04:48 +0200

    Propagate event on skin changes to google analytics

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -105,6 +105,15 @@ elmEyes.ports.output.subscribe(function (elmMessage) {
 elmEyes.ports.outputState.subscribe(function (elmMessage) {
   const [message, body] = elmMessage;
   channel.push(message, body)
+
+  if(ga) {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'change_skin',
+      eventAction: JSON.stringify(body),
+      eventLabel: 'eyes'
+    });
+  }
 });
 
 channel.on("state", payload => {
@@ -151,14 +160,23 @@ theElement2.addEventListener("touchmove", handlerFunction, false);
 theElement3.addEventListener("touchmove", handlerFunction, false);
 
 function handlerFunction(event) {
-const x = Math.floor(event.changedTouches[0].pageX);
-const y = Math.floor(event.changedTouches[0].pageY);
-const position = {
-  x,
-  y
+  const x = Math.floor(event.changedTouches[0].pageX);
+  const y = Math.floor(event.changedTouches[0].pageY);
+  const position = {
+    x,
+    y
+  }
+  channel.push("walk", position)
 }
-channel.push("walk", position)
-}
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-96370760-1', 'auto');
+ga('send', 'pageview');
 
 
 export default socket
