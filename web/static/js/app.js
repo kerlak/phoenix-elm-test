d90049b5e3ce05f376a7355ee1d21e83f77e72a6
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2017-03-29 06:01:41 +0200

    Becoming unicorns

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,32 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 import socket from "./socket"
+
+const elixir_css = "background-color: #762ca7; color: white; font-size: 1.2em;";
+const phoenix_css = "background-color: #ef4734; color: white; font-size: 1.2em;";
+const elm_css = "background-color: #60B5CC; color: white; font-size: 1.2em;";
+const unicorn_css = "background: linear-gradient(rgba(255, 0, 0, 1), rgba(255, 255, 0, 1), rgba(0, 255, 0, 1), rgba(0, 255, 255, 1), rgba(0, 0, 255, 1), rgba(255, 0, 255, 1), rgba(255, 0, 0, 1)); color: white; font-size: 1.2em;";
+const secrets_css = "font-size:0.3em;";
+const super_secrets_css = "font-size:0.1em;";
+const hyper_secrets_css = "font-size:0.0em;";
+
+console.log("Made with:");
+console.log("%c     * Elixir", elixir_css);
+console.log("%c     * Phoenix", phoenix_css);
+console.log("%c     * Elm-lang", elm_css);
+console.log("   and");
+console.log("%c     * Unicorn powder", unicorn_css);
+
+console.log("%cDiscover all the secrets…", secrets_css);
+console.log("%cCongrats! Please tell us how did you find this text sending an email to: undercover@bluetab.net", super_secrets_css);
+
+console.log("%cvariable_secreta", hyper_secrets_css);
+
+const showNewTitle = function() {
+  const title = document.title.replace("/", "");
+  let position = document.title.split("/")[0].length;
+  position = (position + 1) % (title.length + 1);
+  const newTitle = title.substr(0,position) + "/" + title.substr(position);
+  document.title = newTitle;
+}
+setInterval(function(){ showNewTitle() }, 300);
